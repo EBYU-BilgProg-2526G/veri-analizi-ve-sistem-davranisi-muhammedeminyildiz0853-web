585abc7ac8fba76d1ff3fb2ccd856d2d1b6eb6c8
muhammedeminyildiz0853-web <muhammedeminyildiz0853@gmail.com>
2026-01-19 14:22:51 +0300

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,39 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Temel sinyal analiz fonksiyonları
-"""
+
+import numpy as np
 
 def sampling_rate(t):
-    """
-    Zaman vektöründen örnekleme frekansını hesaplar
-
-    Parametre:
-        t : zaman vektörü
-
-    Dönen:
-        fs : örnekleme frekansı (Hz)
-    """
-    # TODO:
-    # 1. ardışık iki zaman örneği arasındaki farkı hesapla
-    # 2. fs = 1 / dt
-    pass
+    dt = t[1] - t[0]
+    return 1 / dt
 
 
 def basic_stats(x):
-    """
-    Sinyal için temel istatistikleri hesaplar
-
-    Parametre:
-        x : sinyal vektörü
-
-    Dönen:
-        stats (dict):
-            mean
-            std
-            rms
-            min
-            max
-    """
-    # TODO:
-    # numpy kullanarak gerekli istatistikleri hesapla
-    pass
+    return {
+        "mean": np.mean(x),
+        "std": np.std(x),
+        "rms": np.sqrt(np.mean(x**2)),
+        "min": np.min(x),
+        "max": np.max(x)
+    }
