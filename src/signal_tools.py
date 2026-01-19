b8afbba915f1e737554f83d18564322f44268a1d
muhammedeminyildiz0853-web <muhammedeminyildiz0853@gmail.com>
2026-01-19 14:26:56 +0300

    Update signal_tools.py

--- src/signal_tools.py
+++ src/signal_tools.py
@@ -1,21 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Basit sinyal işleme fonksiyonları
-"""
-
 def moving_average(x, window_size):
-    """
-    Hareketli ortalama filtresi
+    y = []
 
-    Parametreler:
-        x           : sinyal vektörü
-        window_size : pencere uzunluğu (int)
+    for i in range(len(x)):
+        if i < window_size:
+            y.append(sum(x[:i+1]) / (i+1))
+        else:
+            y.append(sum(x[i-window_size+1:i+1]) / window_size)
 
-    Dönen:
-        y : filtrelenmiş sinyal
-    """
-    # TODO:
-    # 1. her örnek için pencereyi belirle
-    # 2. pencere içindeki ortalamayı hesapla
-    # 3. sonucu yeni bir listeye yaz
-    pass
+    return y
